3aaf9ae509ea8e674afbc4bdd7b1ea5b5a1d18ce
maxmon <fjymbjlzgsj@gmail.com>
2015-03-28 15:34:19 +0800

    modify

--- pub-init.sh
+++ pub-init.sh
@@ -3,6 +3,7 @@
 #create origin branch gh-pages
 git checkout -b gh-pages
 git push -u origin gh-pages
+git checkout master
 git branch -D gh-pages
 #must mkdir a new folder or cause a remote repository name conflict
 mkdir ../gitbook
