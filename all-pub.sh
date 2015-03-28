c3ec4adf8ddd81e7a29ffa82bb80427fb38c7c40
maxmon <fjymbjlzgsj@gmail.com>
2015-03-28 16:45:19 +0800

    add all sh

--- all-pub.sh
+++ all-pub.sh
@@ -1,63 +1,45 @@
 cd ../android
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../cn-stock
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../cool
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../core-invest
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../dairy
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../git
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../google-android-note
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../lisp
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
 cd ../us-stock
 
 ##command
-git commit -am "0328"
-git push -u origin master
 pub.sh
 
---
+++ all-pull.sh
@@ -0,0 +1,54 @@
+cd ../android
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../cn-stock
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../cool
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../core-invest
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../dairy
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../git
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../google-android-note
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../lisp
+
+##command
+git fetch --al
+git rebase origin/master
+
+cd ../us-stock
+
+##command
+git fetch --al
+git rebase origin/master
+
