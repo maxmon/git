b92d9c8c4c5d0f9bb758037df3557038fcd160a7
maxmon <fjymbjlzgsj@gmail.com>
2015-03-30 13:02:34 +0800

    ""[Da.sh

--- all.sh
+++ all.sh
@@ -4,32 +4,32 @@ read command
 cd ../android
 
 ##command
-
+${command}
 
 cd ../cn-stock
 
 ##command
-
+${command}
 
 cd ../cn-stock-dairy
 
 ##command
-
+${command}
 
 cd ../cool
 
-##command
-
+#${command}#command
+${command}
 
 cd ../core-invest
 
 ##command
-
+${command}
 
 cd ../dairy
 
 ##command
-
+${command}
 
 cd ../git
 
@@ -40,15 +40,15 @@ ${command}
 cd ../google-android-note
 
 ##command
-
+${command}
 
 cd ../lisp
 
 ##command
-
+${command}
 
 cd ../us-stock
 
 ##command
-
+${command}
 
