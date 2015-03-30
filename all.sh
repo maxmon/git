8318557e1ef37b3533c7050ca7758a9de946268f
maxmon <fjymbjlzgsj@gmail.com>
2015-03-30 13:01:12 +0800

    ""[Dall.sh

---
+++ all.sh
@@ -0,0 +1,54 @@
+echo "input command:"
+read command
+
+cd ../android
+
+##command
+
+
+cd ../cn-stock
+
+##command
+
+
+cd ../cn-stock-dairy
+
+##command
+
+
+cd ../cool
+
+##command
+
+
+cd ../core-invest
+
+##command
+
+
+cd ../dairy
+
+##command
+
+
+cd ../git
+
+##command
+${command}
+
+
+cd ../google-android-note
+
+##command
+
+
+cd ../lisp
+
+##command
+
+
+cd ../us-stock
+
+##command
+
+
