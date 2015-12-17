71b9b94ac003033e854e9e337e05becc22848ab2
maxmon <fjymbjlzgsj@gmail.com>
2015-12-18 01:40:33 +0800

    rm init from ignore

---
+++ init/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#init repo
+rm -rf README.md
+echo "on README.md >>"
+read input
+echo "$input" >> README.md
+touch LICENSE
+echo "\ninit.sh\np.sh\nap.sh\nfr.sh"  >> .gitignore
+
+#init git
+git init
+echo "repository url >>"
+read remote_url
+git remote add origin "$remote_url"
+
+#git script
+rm -rf p.sh
+rm -rf ap.sh
+rm -rf fr.sh
+echo "#!/bin/bash\n\ngit status\n\necho \"commit message >>\"\nread input\ngit commit -am \"\$input\"\n\ngit push -u origin master" >> p.sh
+echo "#!/bin/bash\n\ngit fetch --al\ngit rebase origin/master" >> fr.sh
+echo "#!/bin/bash\n\ngit add .\ngit status\n\necho \"commit message >>\"\nread input\ngit commit -am \"\$input\"\n\ngit push -u origin master" >> ap.sh
+
+chmod 777 ./*.sh
+
+#commit 
+git add .
+git status
+git commit -am "init"
+git push -u origin master
+
+
