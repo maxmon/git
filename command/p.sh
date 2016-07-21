c436ef478c71d426cd8483c1e425a38481b2a81e
maxmon <fjymbjlzgsj@gmail.com>
2016-07-21 15:37:08 +0800

    auto commit 2016-07-21

--- command/p.sh
+++ command/p.sh
@@ -11,4 +11,14 @@ fi
 
 git commit -am "$msg"
 
+git push origin HEAD:master#!/bin/bash
+git add .
+msg=""
+if [ -z "$1"]; then
+	msg="auto commit 2016-07-19"
+else
+	msg="$1"
+fi
+git commit -am "$msg"
 git push origin HEAD:master
+git status
